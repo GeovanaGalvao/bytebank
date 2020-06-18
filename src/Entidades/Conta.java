66d5cc038c77d907d9c21f7fdb179aaa67702959
GeovanaGalvao <gngalvao732@gmail.com>
2020-06-17 22:38:34 -0300

    Implementação de métodos saca, deposita e transfere

--- src/Entidades/Conta.java
+++ src/Entidades/Conta.java
@@ -6,4 +6,28 @@ public class Conta {
 	public int agencia;
 	public int numero;
 	public String titular;
+	
+	public void deposita(double valor) {
+		saldo += valor;
+	}
+	
+	public boolean saca(double valor) {
+		
+		if (saldo >= valor) {
+			saldo -= valor;
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public boolean transfere(double valor, Conta destino) {
+		
+		if (saca(valor) == true) {
+			destino.deposita(valor);
+			return true;
+		}
+		
+		return false;
+	}
 }
---
+++ src/Programa/TestaMetodo.java
@@ -0,0 +1,38 @@
+package Programa;
+
+import Entidades.Conta;
+
+public class TestaMetodo {
+
+	public static void main(String[] args) {
+		
+		Conta conta = new Conta();
+		conta.deposita(50.00);
+		System.out.println("Saldo atual: " + conta.saldo);
+		conta.saca(100.00);
+		System.out.println("Conseguiu retirar 100.0? " + conta.saca(100.00));
+		conta.deposita(50.00);
+		System.out.println("Deposito de 50.0");
+		System.out.println("Saldo atual: " + conta.saldo);
+		System.out.println("Conseguiu retirar 100.0? " + conta.saca(100.00));
+		System.out.println("Saldo atual: " + conta.saldo);		
+		System.out.println("--------------------------------");
+		
+		Conta contaTeste = new Conta();
+		contaTeste.deposita(1000);
+		
+		if(contaTeste.transfere(300,conta)) {
+			System.out.println("Saldo atual da conta: " + conta.saldo);
+			System.out.println("Saldo atual da contaTeste: " + contaTeste.saldo);
+		} else
+			System.out.println("Saldo insuficiente.");
+		
+		System.out.println("--------------------------------");
+		
+		if(conta.transfere(500,contaTeste)) {
+			System.out.println("Saldo atual da conta: " + conta.saldo);
+			System.out.println("Saldo atual da contaTeste: " + contaTeste.saldo);
+		} else
+			System.out.println("Saldo insuficiente.");
+	}
+}
